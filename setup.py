dfc59b1127e57c07868eb54c7f1b338c420aa26e
Tali Herzka <tali@herzka.com>
2015-03-16 20:30:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='transcriptic',
-    version='1.0.0',
+    version='1.0.1',
     py_modules=['transcriptic'],
     install_requires=[
         'Click',
