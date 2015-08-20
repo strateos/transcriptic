9a3f50d276aaf306131fee6b6583d61f0a7506ab
Tali Herzka <tali@herzka.com>
2015-08-20 10:23:28 -0700

    final version with bug fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='transcriptic',
-    version='1.3.2',
+    version='1.3.12',
     py_modules=['transcriptic'],
     install_requires=[
         'Click',
