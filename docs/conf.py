4365e9740e51cda54fca0b9cbdaf44b7f1082d58
Tali Herzka <tali@herzka.com>
2015-10-28 14:08:16 -0700

    try to fix build failure due to matplotlib

--- docs/conf.py
+++ docs/conf.py
@@ -313,3 +313,16 @@ if not on_rtd or on_rtd:  # only import and set the theme if we're building docs
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
+                'sklearn.grid_search', 'sklearn.externals', 'plotly', 'plotly.graph_objs',
+                'matplotlib.gridspec', 'scikit-learn']
+
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
