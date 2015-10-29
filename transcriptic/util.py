0dabd3fe47118b86768a77240d910fdf1b2fde1f
yangchoo <yangchoo@transcriptic.com>
2015-10-29 16:52:56 -0700

    Refactored plate read and resolved dependencies and other issues

--- transcriptic/util.py
+++ transcriptic/util.py
@@ -1,3 +1,5 @@
+import re
+
 def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
