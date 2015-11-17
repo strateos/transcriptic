91bbc5656284734418edf143fbe47fc0bc2b2ae9
yangchoo <yangchoo@transcriptic.com>
2015-11-17 10:41:13 -0800

    Quick fix to spectrophotometry due to `dataset` output change

--- transcriptic/analysis/spectrophotometry.py
+++ transcriptic/analysis/spectrophotometry.py
@@ -51,7 +51,7 @@ class PlateRead(object):
         self.params["plate"] = plate_info_dict
 
         # Get dataset and parse into DataFrame
-        data_dict = get_dataset(self.dataset.attributes["id"])
+        data_dict = get_dataset(self.dataset.attributes["id"]).attributes
         df_dict = {}
         well_count = self.dataset.attributes["container_type"]["well_count"]
         col_count = self.dataset.attributes["container_type"]["col_count"]
