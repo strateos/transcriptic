e0c63e98f7345af2c47c943d2f42c5796f212cb9
rhys <rhysormond@gmail.com>
2018-12-13 15:41:01 -0800

    Explore reworking routes

--- transcriptic/routes.py
+++ transcriptic/routes.py
@@ -4,185 +4,177 @@ Note: {api_root} and {org_id} are automatically supplied in the Connection.get_r
 """
 
 
-def create_project(api_root, org_id):
-    return "{api_root}/{org_id}".format(**locals())
+class Routes(object):
+    def __init__(self, api_root, org_id):
+        self.api_root = api_root
+        self.org_id = org_id
 
+    def _get(self, route, **kwargs):
+        kwargs.update(self.__dict__)
+        return route.format(**kwargs)
 
-def delete_project(api_root, org_id, project_id):
-    return "{api_root}/{org_id}/{project_id}".format(**locals())
+    def create_project(self):
+        return self._get("{api_root}/{org_id}")
 
+    def delete_project(self, project_id):
+        return self._get("{api_root}/{org_id}/{project_id}", project_id=project_id)
 
-def archive_project(api_root, org_id, project_id):
-    return "{api_root}/{org_id}/{project_id}".format(**locals())
+    def archive_project(self, project_id):
+        return self._get("{api_root}/{org_id}/{project_id}", project_id=project_id)
 
+    def get_project(self, project_id):
+        return self._get("{api_root}/{org_id}/{project_id}", project_id=project_id)
 
-def get_project(api_root, org_id, project_id):
-    return "{api_root}/{org_id}/{project_id}".format(**locals())
+    def get_projects(self):
+        return self._get("{api_root}/{org_id}/?q=&per_page=500")
 
+    def get_project_runs(self, project_id):
+        return self._get("{api_root}/{org_id}/{project_id}/runs", project_id=project_id)
 
-def get_projects(api_root, org_id):
-    return "{api_root}/{org_id}/?q=&per_page=500".format(**locals())
+    def create_package(self):
+        return self._get("{api_root}/{org_id}/packages")
 
+    def delete_package(self, package_id):
+        return self._get("{api_root}/{org_id}/packages/{package_id}", package_id=package_id)
 
-def get_project_runs(api_root, org_id, project_id):
-    return "{api_root}/{org_id}/{project_id}/runs".format(**locals())
+    def get_package(self, package_id):
+        return self._get("{api_root}/{org_id}/packages/{package_id}", package_id=package_id)
 
+    def get_packages(self):
+        return self._get("{api_root}/{org_id}/packages/")
 
-def create_package(api_root, org_id):
-    return "{api_root}/{org_id}/packages".format(**locals())
+    def get_protocols(self):
+        return self._get("{api_root}/{org_id}/protocols")
 
+    def launch_protocol(self, protocol_id):
+        return self._get("{api_root}/{org_id}/protocols/{protocol_id}/launch", protocol_id=protocol_id)
 
-def delete_package(api_root, org_id, package_id):
-    return "{api_root}/{org_id}/packages/{package_id}".format(**locals())
+    def get_launch_request(self, protocol_id, launch_request_id):
+        return self._get(
+            "{api_root}/{org_id}/protocols/{protocol_id}/launch/{launch_request_id}",
+            protocol_id=protocol_id,
+            launch_request_id=launch_request_id
+        )
 
+    def post_release(self, package_id):
+        return self._get("{api_root}/{org_id}/packages/{package_id}/releases/", package_id=package_id)
 
-def get_package(api_root, org_id, package_id):
-    return "{api_root}/{org_id}/packages/{package_id}".format(**locals())
+    def get_release_status(self, package_id, release_id, timestamp):
+        return self._get(
+            "{api_root}/{org_id}/packages/{package_id}/releases/{release_id}?_={timestamp}",
+            package_id=package_id,
+            release_id=release_id,
+            timestamp=timestamp
+        )
 
+    def query_kits(self, query):
+        return self._get("{api_root}/_commercial/kits?q={query}&per_page=1000&full_json=true", query=query)
 
-def get_packages(api_root, org_id):
-    return "{api_root}/{org_id}/packages/".format(**locals())
+    def query_resources(self, query):
+        return self._get("{api_root}/_commercial/resources?q={query}&per_page=1000", query=query)
 
+    def query_inventory(self, query, page=0):
+        return self._get("{api_root}/{org_id}/inventory/samples?q={query}&per_page=75&page={page}", query=query,
+                         page=page)
 
-def get_protocols(api_root, org_id):
-    return "{api_root}/{org_id}/protocols".format(**locals())
+    def get_quick_launch(self, project_id, quick_launch_id):
+        return self._get(
+            "{api_root}/{org_id}/{project_id}/runs/quick_launch/{quick_launch_id}",
+            project_id=project_id,
+            quick_launch_id=quick_launch_id
+        )
 
+    def create_quick_launch(self, project_id):
+        return self._get("{api_root}/{org_id}/{project_id}/runs/quick_launch", project_id=project_id)
 
-def launch_protocol(api_root, org_id, protocol_id):
-    return "{api_root}/{org_id}/protocols/{protocol_id}/launch".format(**locals())
+    def resolve_quick_launch_inputs(self, project_id, quick_launch_id):
+        return self._get(
+            "{api_root}/{org_id}/{project_id}/runs/quick_launch/{quick_launch_id}/resolve_inputs",
+            project_id=project_id,
+            quick_launch_id=quick_launch_id
+        )
 
+    def login(self):
+        return self._get("{api_root}/users/sign_in")
 
-def get_launch_request(api_root, org_id, protocol_id, launch_request_id):
-    return "{api_root}/{org_id}/protocols/{protocol_id}/launch/{launch_request_id}".format(**locals())
+    def get_organizations(self):
+        return self._get("{api_root}/organizations")
 
+    def get_organization(self):
+        return self._get("{api_root}/{org_id}")
 
-def post_release(api_root, org_id, package_id):
-    return "{api_root}/{org_id}/packages/{package_id}/releases/".format(**locals())
+    def deref_route(self):
+        return self._get("{api_root}/-/{obj_id}")
 
+    def analyze_run(self):
+        return self._get("{api_root}/{org_id}/analyze_run")
 
-def get_release_status(api_root, org_id, package_id, release_id, timestamp):
-    return "{api_root}/{org_id}/packages/{package_id}/releases/{release_id}?_={timestamp}".format(**locals())
+    def analyze_launch_request(self):
+        return self._get("{api_root}/{org_id}/analyze_run")
 
+    def submit_run(self, project_id):
+        return self._get("{api_root}/{org_id}/{project_id}/runs", project_id=project_id)
 
-def query_kits(api_root, query):
-    return "{api_root}/_commercial/kits?q={query}&per_page=1000&full_json=true".format(**locals())
+    def submit_launch_request(self, project_id):
+        return self._get("{api_root}/{org_id}/{project_id}/runs", project_id=project_id)
 
+    def dataset_short(self, data_id):
+        return self._get("{api_root}/datasets/{data_id}.json", data_id=data_id)
 
-def query_resources(api_root, query):
-    return "{api_root}/_commercial/resources?q={query}&per_page=1000".format(**locals())
+    def dataset(self, data_id, key):
+        return self._get("{api_root}/datasets/{data_id}.json?key={key}", data_id=data_id, key=key)
 
+    def datasets(self, project_id, run_id):
+        return self._get("{api_root}/{org_id}/{project_id}/runs/{run_id}/data", project_id=project_id, run_id=run_id)
 
-def query_inventory(api_root, org_id, query, page=0):
-    return "{api_root}/{org_id}/inventory/samples?q={query}&per_page=75&page={page}".format(**locals())
+    def get_uploads(self, key):
+        return self._get("{api_root}/upload/url_for?key={key}", key=key)
 
+    def upload(self):
+        return self._get("{api_root}/api/uploads")
 
-def get_quick_launch(api_root, org_id, project_id, quick_launch_id):
-    return "{api_root}/{org_id}/{project_id}/runs/quick_launch/{quick_launch_id}".format(**locals())
+    def upload_datasets(self):
+        return self._get("{api_root}/api/datasets")
 
+    def preview_protocol(self):
+        return self._get("{api_root}/runs/preview")
 
-def create_quick_launch(api_root, org_id, project_id):
-    return "{api_root}/{org_id}/{project_id}/runs/quick_launch".format(**locals())
+    def preview_protocol_embed(self, preview_id):
+        return self._get("{api_root}/runs/preview/{preview_id}.embed", preview_id=preview_id)
 
+    def view_data(self, data_id):
+        return self._get("{api_root}/datasets/{data_id}.embed", data_id=data_id)
 
-def resolve_quick_launch_inputs(api_root, org_id, project_id, quick_launch_id):
-    return "{api_root}/{org_id}/{project_id}/runs/quick_launch/" \
-           "{quick_launch_id}/resolve_inputs".format(**locals())
+    def view_run(self, project_id, run_id):
+        return self._get("{api_root}/{org_id}/{project_id}/runs/{run_id}.embed", project_id=project_id, run_id=run_id)
 
+    def view_instruction(self, project_id, run_id, instruction_id):
+        return self._get(
+            "{api_root}/{org_id}/{project_id}/runs/{run_id}/instructions/{instruction_id}.embed",
+            project_id=project_id,
+            run_id=run_id,
+            instruction_id=instruction_id
+        )
 
-def login(api_root):
-    return "{api_root}/users/sign_in".format(**locals())
+    def view_raw_image(self, data_id):
+        return self._get("{api_root}/-/{data_id}.raw", data_id=data_id)
 
+    def get_data_zip(self, data_id):
+        return self._get("{api_root}/-/{data_id}.zip", data_id=data_id)
 
-def get_organizations(api_root):
-    return "{api_root}/organizations".format(**locals())
+    def monitoring_data(self, data_type, instruction_id, grouping=None, start_time=None, end_time=None):
+        base_route = "{api_root}/sensor_data/{data_type}?instruction_id={instruction_id}"
+        base_route += "&grouping={grouping}" if grouping else ""
+        base_route += "&start_time={start_time}" if start_time else ""
+        base_route += "&end_time={end_time}" if end_time else ""
+        return self._get(
+            base_route,
+            data_type=data_type,
+            instruction_id=instruction_id,
+            grouping=grouping,
+            start_time=start_time,
+            end_time=end_time
+        )
 
-
-def get_organization(api_root, org_id):
-    return "{api_root}/{org_id}".format(**locals())
-
-
-def deref_route(api_root, obj_id):
-    return "{api_root}/-/{obj_id}".format(**locals())
-
-
-def analyze_run(api_root, org_id):
-    return "{api_root}/{org_id}/analyze_run".format(**locals())
-
-
-def analyze_launch_request(api_root, org_id):
-    return "{api_root}/{org_id}/analyze_run".format(**locals())
-
-
-def submit_run(api_root, org_id, project_id):
-    return "{api_root}/{org_id}/{project_id}/runs".format(**locals())
-
-
-def submit_launch_request(api_root, org_id, project_id):
-    return "{api_root}/{org_id}/{project_id}/runs".format(**locals())
-
-
-def dataset_short(api_root, data_id):
-    return "{api_root}/datasets/{data_id}.json".format(**locals())
-
-
-def dataset(api_root, data_id, key):
-    return "{api_root}/datasets/{data_id}.json?key={key}".format(**locals())
-
-
-def datasets(api_root, org_id, project_id, run_id):
-    return "{api_root}/{org_id}/{project_id}/runs/{run_id}/data".format(**locals())
-
-
-def get_uploads(api_root, key):
-    return "{api_root}/upload/url_for?key={key}".format(**locals())
-
-
-def upload(api_root):
-    return "{api_root}/api/uploads".format(**locals())
-
-
-def upload_datasets(api_root):
-    return "{api_root}/api/datasets".format(**locals())
-
-
-def preview_protocol(api_root):
-    return "{api_root}/runs/preview".format(**locals())
-
-
-def preview_protocol_embed(api_root, preview_id):
-    return "{api_root}/runs/preview/{preview_id}.embed".format(**locals())
-
-
-def view_data(api_root, data_id):
-    return "{api_root}/datasets/{data_id}.embed".format(**locals())
-
-
-def view_run(api_root, org_id, project_id, run_id):
-    return "{api_root}/{org_id}/{project_id}/runs/{run_id}.embed".format(**locals())
-
-
-def view_instruction(api_root, org_id, project_id, run_id, instruction_id):
-    return "{api_root}/{org_id}/{project_id}/runs/{run_id}/instructions/{instruction_id}.embed".format(**locals())
-
-
-def view_raw_image(api_root, data_id):
-    return "{api_root}/-/{data_id}.raw".format(**locals())
-
-
-def get_data_zip(api_root, data_id):
-    return "{api_root}/-/{data_id}.zip".format(**locals())
-
-
-def monitoring_data(api_root, data_type, instruction_id, grouping=None, start_time=None, end_time=None):
-    base_route = "{api_root}/sensor_data/{data_type}?instruction_id={instruction_id}".format(**locals())
-    if grouping:
-        base_route += "&grouping={grouping}".format(**locals())
-    if start_time:
-        base_route += "&start_time={start_time}".format(**locals())
-    if end_time:
-        base_route += "&end_time={end_time}".format(**locals())
-    return base_route
-
-
-def get_payment_methods(api_root, org_id):
-    return "{api_root}/{org_id}/payment_methods".format(**locals())
+    def get_payment_methods(self):
+        return self._get("{api_root}/{org_id}/payment_methods")
