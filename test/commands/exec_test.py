d6d38c92f6247a191eb90baf0485111f634b09c0
Scott Antipa <scottantipa@gmail.com>
2021-02-11 17:13:33 +0100

    use isolated fs for all tests

--- test/commands/exec_test.py
+++ test/commands/exec_test.py
@@ -13,10 +13,6 @@ def bool_success_res():
     return {"success": True}
 
 
-def good_autoprotocol_file():
-    return "test/autoprotocol/singleTransfer.json"
-
-
 def mock_api_endpoint():
     return "foo.bar.baz"
 
@@ -28,11 +24,9 @@ def test_good_autoprotocol(monkeypatch):
     monkeypatch.setattr(requests, "post", mockpost)
     runner = CliRunner()
     with runner.isolated_filesystem():
-        with open('ap.json', 'w') as f:
-            f.write('{}') # any valid json works
-        result = runner.invoke(
-            cli, ["exec", 'ap.json', "-a", mock_api_endpoint()]
-        )
+        with open("ap.json", "w") as f:
+            f.write("{}")  # any valid json works
+        result = runner.invoke(cli, ["exec", "ap.json", "-a", mock_api_endpoint()])
         assert result.exit_code == 0
         assert (
             f"Success. View {mock_api_endpoint()} to see the scheduling outcome."
@@ -40,41 +34,45 @@ def test_good_autoprotocol(monkeypatch):
         )
 
 
-# def test_bad_autoprotocol():
-#     runner = CliRunner()
-#     result = runner.invoke(cli, ["exec", "bad-file-handle", "-a", mock_api_endpoint()])
-#     assert result.exit_code != 0
-#     assert "Invalid value for '[AUTOPROTOCOL]': Could not open file" in result.output
+def test_bad_autoprotocol():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["exec", "bad-file-handle", "-a", mock_api_endpoint()])
+    assert result.exit_code != 0
+    assert "Invalid value for '[AUTOPROTOCOL]': Could not open file" in result.output
 
 
-# def test_bad_deviceset():
-#     runner = CliRunner()
-#     result = runner.invoke(
-#         cli,
-#         [
-#             "exec",
-#             good_autoprotocol_file(),
-#             "--device-set",
-#             "bad-file-handle",
-#             "-a",
-#             mock_api_endpoint(),
-#         ],
-#     )
-#     assert result.exit_code != 0
-#     assert (
-#         "Invalid value for '--device-set' / '-d': Could not open file: bad-file-handle:"
-#         in result.output
-#     )
+def test_bad_deviceset():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open("ap.json", "w") as f:
+            f.write("{}")  # any valid json works
+        result = runner.invoke(
+            cli,
+            [
+                "exec",
+                "ap.json",
+                "--device-set",
+                "bad-file-handle",
+                "-a",
+                mock_api_endpoint(),
+            ],
+        )
+        assert result.exit_code != 0
+        assert (
+            "Invalid value for '--device-set' / '-d': Could not open file: bad-file-handle:"
+            in result.output
+        )
 
 
-# def test_bad_api_response(monkeypatch):
-#     def mockpost(*args, **kwargs):
-#         return MockResponse(0, "not-json", "not-json")
+def test_bad_api_response(monkeypatch):
+    def mockpost(*args, **kwargs):
+        return MockResponse(0, "not-json", "not-json")
 
-#     monkeypatch.setattr(requests, "post", mockpost)
-#     runner = CliRunner()
-#     result = runner.invoke(
-#         cli, ["exec", good_autoprotocol_file(), "-a", mock_api_endpoint()]
-#     )
-#     assert result.exit_code == 0
-#     assert "Error: " in result.output
+    monkeypatch.setattr(requests, "post", mockpost)
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open("ap.json", "w") as f:
+            f.write("{}")  # any valid json works
+        result = runner.invoke(cli, ["exec", "ap.json", "-a", mock_api_endpoint()])
+        assert result.exit_code == 0
+        assert "Error: " in result.output
