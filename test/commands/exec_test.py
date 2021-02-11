41d85ccd3d49f4b775333ebb1e311692fb23b550
Scott Antipa <scottantipa@gmail.com>
2021-02-11 19:15:46 +0100

    use our cli_runner in all my tests

--- test/commands/exec_test.py
+++ test/commands/exec_test.py
@@ -23,57 +23,53 @@ def test_good_autoprotocol(cli_test_runner, monkeypatch, tmpdir_factory):
     with open(str(path), "w") as f:
         f.write("{}")  # any valid json works
 
-    result = cli_test_runner.invoke(cli, ["exec", str(path), "-a", mock_api_endpoint()], catch_exceptions=False)
+    result = cli_test_runner.invoke(cli, ["exec", str(path), "-a", mock_api_endpoint()])
     assert result.exit_code == 0
     assert (
         f"Success. View {mock_api_endpoint()} to see the scheduling outcome."
         in result.output
     )
 
+def test_bad_autoprotocol(cli_test_runner):
+    result = cli_test_runner.invoke(cli, ["exec", "bad-file-handle", "-a", mock_api_endpoint()])
+    assert result.exit_code != 0
+    assert "Invalid value for '[AUTOPROTOCOL]': Could not open file" in result.stderr
 
-# def test_bad_autoprotocol():
-#     runner = CliRunner()
-#     result = runner.invoke(cli, ["exec", "bad-file-handle", "-a", mock_api_endpoint()])
-#     assert result.exit_code != 0
-#     assert "Invalid value for '[AUTOPROTOCOL]': Could not open file" in result.output
+def test_bad_deviceset(cli_test_runner, tmpdir_factory):
+    # make autoprotocol file
+    path = tmpdir_factory.mktemp("foo").join("ap.json")
+    with open(str(path), "w") as f:
+        f.write("{}")  # any valid json works
 
-# def test_bad_deviceset(tmpdir_factory):
-#     runner = CliRunner()
-#     # make autoprotocol file
-#     path = tmpdir_factory.mktemp("foo").join("ap.json")
-#     with open(str(path), "w") as f:
-#         f.write("{}")  # any valid json works
-
-#     result = runner.invoke(
-#         cli,
-#         [
-#             "exec",
-#             str(path),
-#             "--device-set",
-#             "bad-file-handle",
-#             "-a",
-#             mock_api_endpoint(),
-#         ],
-#     )
-#     assert result.exit_code != 0
-#     assert (
-#         "Invalid value for '--device-set' / '-d': Could not open file: bad-file-handle:"
-#         in result.output
-#     )
+    result = cli_test_runner.invoke(
+        cli,
+        [
+            "exec",
+            str(path),
+            "--device-set",
+            "bad-file-handle",
+            "-a",
+            mock_api_endpoint(),
+        ],
+    )
+    assert result.exit_code != 0
+    assert (
+        "Invalid value for '--device-set' / '-d': Could not open file: bad-file-handle:"
+        in result.stderr
+    )
 
 
-# def test_bad_api_response(monkeypatch, tmpdir_factory):
-#     def mockpost(*args, **kwargs):
-#         return MockResponse(0, "not-json", "not-json")
+def test_bad_api_response(cli_test_runner, monkeypatch, tmpdir_factory):
+    def mockpost(*args, **kwargs):
+        return MockResponse(0, "not-json", "not-json")
 
-#     monkeypatch.setattr(requests, "post", mockpost)
-#     runner = CliRunner()
+    monkeypatch.setattr(requests, "post", mockpost)
 
-#     # make autoprotocol file
-#     path = tmpdir_factory.mktemp("foo").join("ap.json")
-#     with open(str(path), "w") as f:
-#         f.write("{}")  # any valid json works
+    # make autoprotocol file
+    path = tmpdir_factory.mktemp("foo").join("ap.json")
+    with open(str(path), "w") as f:
+        f.write("{}")  # any valid json works
 
-#     result = runner.invoke(cli, ["exec", str(path), "-a", mock_api_endpoint()])
-#     assert result.exit_code == 0
-#     assert "Error: " in result.output
+    result = cli_test_runner.invoke(cli, ["exec", str(path), "-a", mock_api_endpoint()])
+    assert result.exit_code == 0
+    assert "Error: " in result.stderr
