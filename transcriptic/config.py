d3f9817ea453bf02dd6d0a701768fc2010562a92
aaronstrateos <aaron.karlsberg@strateos.com>
2021-07-16 13:20:50 -0700

    lint

--- transcriptic/config.py
+++ transcriptic/config.py
@@ -722,9 +722,8 @@ class Connection(object):
             "protocol_id": protocol_id,
             "test_mode": test_mode,
             "payment_method_id": payment_method_id,
-            "predecessor_id": predecessor_id
+            "predecessor_id": predecessor_id,
         }
-        
         data = {k: v for k, v in payload.items() if v is not None}
         return self.post(
             self.get_route("submit_launch_request", project_id=project_id),
