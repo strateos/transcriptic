716deb2a6c7bbe6e3406063cf91d19aea069a965
EribertoLopez <eriberto.lopez@strateos.com>
2022-06-13 10:23:08 -0700

    update: connnection token logic

--- transcriptic/config.py
+++ transcriptic/config.py
@@ -156,15 +156,15 @@ class Connection(object):
                 )
             self.session.headers["Cookie"] = None
             self.email = email
-            if token is not None:
-                self.token = token
-                if bearer_token is not None:
+            if bearer_token is not None:
+                if token is not None:
                     warnings.warn(
                         "User token and bearer token authentication"
-                        "is mutually exclusive. Ignoring bearer token"
+                        "is mutually exclusive. Ignoring user token"
                     )
-            elif bearer_token is not None:
                 self.bearer_token = bearer_token
+            elif token is not None:
+                self.token = token
             self.update_session_auth()
 
         # Initialize feature groups
